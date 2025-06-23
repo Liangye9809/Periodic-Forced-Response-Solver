e641f1247135c81953ae3b4fffd669b8d5deb123
Liangye9809 <lly_2324@163.com>
2025-06-23 15:31:45 +0200

    6/23 before meeting

--- two-solid system/test.m
+++ two-solid system/test.m
@@ -31,13 +31,20 @@ tic
 testF = g(rand(256,17), 0);
 toc
 %%
-disp(['dt   ','nsteps   ','max(dtA) ','max(exp(dtA))']);
-for i  = 0:5
+clc
+disp(['dt   ','nsteps   ','max(dtA) ','max(exp(dtA))    ', 'max(abs(eig(exp(dtA))))    ', 'max(abs(eigEuler))  ', 'max(abs(eigRK2))']);
+for i  = 0:10
     nsteps = 100 * 10^i;
     testdt = T / nsteps;
     testdtA = testdt * A;
     testexpdtA = exp(testdtA);
-    disp([testdt, nsteps, max(max(abs(testdtA))), max(max(abs(testexpdtA)))]);
+    e = eig(testexpdtA);
+    eA = eig((eye(34) - testdt * A));
+    RK2R = testdt * A + testdt^2 / 2 * A^2 + eye(34);
+    maxe = max(abs(e));
+    maxeE = max(abs(eA));
+    maxeRK2 = max(abs(eig(RK2R)));
+    disp([testdt, nsteps, max(max(abs(testdtA))), max(max(abs(testexpdtA))), maxe, maxeE, maxeRK2]);
 end
 %% 
 t = -1:0.001:1;
