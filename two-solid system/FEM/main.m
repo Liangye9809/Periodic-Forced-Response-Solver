a331778e28a0627515d47940737dd1a26c9204b2
Liangye9809 <fernandonil404@gmail.com>
2025-10-15 14:33:18 +0200

    FEM

---
+++ two-solid system/FEM/main.m
@@ -0,0 +1,20 @@
+clear
+%Build mass matrix (rotor without cyclic matrices)
+Mij=load('Blade1Anlysis.mas');
+index=Mij(:,1)~=Mij(:,2);
+ii=[Mij(:,1);Mij(index,2)]; %symmetric part
+jj=[Mij(:,2);Mij(index,1)];
+ss=[Mij(:,3);Mij(index,3)];
+clear Mij;
+M=sparse(ii,jj,ss);
+%Build stiffness matrix (rotor without cyclic matrices)
+Kij=load('Blade1Anlysis.sti');
+index=Kij(:,1)~=Kij(:,2);
+ii=[Kij(:,1);Kij(index,2)];
+jj=[Kij(:,2);Kij(index,1)];
+ss=[Kij(:,3);Kij(index,3)];
+clear Kij;
+K=sparse(ii,jj,ss);
+clear ii jj ss;
+[Phi, D] = eigs(K, M, 40, 'smallestabs');
+e = diag(D);
