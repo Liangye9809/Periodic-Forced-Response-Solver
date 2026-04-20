a751a4d8c5148a21bef62ae753c5e7cd7fcb4db5
Liangye9809 <fernandonil404@gmail.com>
2026-04-20 19:42:59 +0200

    4/20

---
+++ mass spring model/Data.m
@@ -0,0 +1,71 @@
+%% FEM matrices for test
+
+FEM.Mee = eye(3);
+FEM.Mcc = eye(3);
+FEM.Mec = zeros(3,3);
+
+
+FEM.Kee = diag([1,2,3]);
+FEM.Kec = -diag([1,2,3]);
+FEM.Kcc = diag([1,2,3]);
+
+FEM.Fe = [1, 0, 1]';
+FEM.Fc = [0, 0 ,0]';
+
+FEM.Pe = [0, 0, -1]';
+FEM.Pc = [0, 0 ,0]';
+
+% xn = - 4 * sin(sin(t)) + 1; % separation to stick
+% xt = 2 * exp(cos(t + 1)) - 3; % separation to stick
+
+%% External  forces
+
+H_F_ext = [0, 0, 1]; % fourier coefficient of f(t)
+
+
+%% HBM parameters
+
+H = 5; % number of harmonics assumption
+N = 2^6; % number of time points per force cycle
+Nx = 1; % number of contact points, means having 4 * 3 = 12 dofs
+Na = 1; % number of CB modes
+xi = 1e-6; 
+
+
+%% Newton Method parameters
+
+epsx = 1e-3;
+epsf = 1e-3;
+maxiter = 100;
+
+%% Coulomb friction
+
+kn = 1; % .* 1e5;
+xn0 = 0;
+mu = [0.1; 0.1]; % .* 1e5;
+kt = [1; 1]; % .* 1e5;
+nloop = 2;
+
+%% preload initial condition for Newton method
+
+xp0 = 0; % if no value defined here, the default value inside is 0
+
+
+%% continuation parameters
+
+
+ds = 0.5;
+maxstep = 20000;
+% omega_0 = 0.81; % 
+% omega_end = 0.87;
+
+
+
+
+
+
+% x0 = 0;
+
+% tx0 = 0; % always 0 when calculating the first point so default inside the code
+% tomega0 = 1; % is defined by lambda0 and lambda_end
+
